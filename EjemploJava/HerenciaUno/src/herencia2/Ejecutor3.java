25e3ab9333660f038a09bd480c3d704e17052385
davidethc <121461595+davidethc@users.noreply.github.com>
2024-06-06 17:41:35 -0500

    jue 6 jun 5:41

--- EjemploJava/HerenciaUno/src/herencia2/Ejecutor3.java
+++ EjemploJava/HerenciaUno/src/herencia2/Ejecutor3.java
@@ -8,44 +8,85 @@ package herencia2;
 import java.util.Locale;
 import java.util.Scanner;
 import herencia1.EstudianteDistancia;
+import herencia1.EstudiantePresencial;
 
 /**
  *
  * @author reroes
  */
 public class Ejecutor3 {
+
     public static void main(String[] args) {
-        // Ingresar por teclado
-        // un objeto de tipo Estudiante Distancia si el usuario ingresa 1 como 
-        // opción
-        // un objeto de tipo Estudiante Presencial si el usuario ingresa 2 como 
-        // opción
-        
         Scanner entrada = new Scanner(System.in);
         entrada.useLocale(Locale.US);
-        System.out.println("Ingrese nombres");
-        String nombres = entrada.nextLine();
-        System.out.println("Ingrese apellidos");
-        String apellidos = entrada.nextLine();
-        System.out.println("Ingrese identificación");
-        String identificacion = entrada.nextLine();
-        System.out.println("Ingrese edad");
-        int edad = entrada.nextInt();
-        System.out.println("Ingrese número de asignaturas");
-        int asignaturas = entrada.nextInt();
-        System.out.println("Ingrese costo asignatura");
-        double costoAsignatura = entrada.nextDouble();
-        EstudianteDistancia estudiante = new EstudianteDistancia();
-        
-        estudiante.establecerNombresEstudiante(nombres);
-        estudiante.establecerApellidoEstudiante(apellidos);
-        estudiante.establecerEdadEstudiante(edad);
-        estudiante.establecerIdentificacionEstudiante(identificacion);
-        estudiante.establecerNumeroAsginaturas(asignaturas);
-        estudiante.establecerCostoAsignatura(costoAsignatura);
-        estudiante.calcularMatriculaDistancia();
-        
-        System.out.printf("%s\n", estudiante);
-        
+
+        System.out.println("Ingrese la opción que desee:");
+        System.out.println("1. Si desea un objeto tipo Estudiante Distancia");
+        System.out.println("2. Si desea un objeto tipo Estudiante Presencial");
+
+        int opcion = entrada.nextInt();
+
+        switch (opcion) {
+            case 1:
+                System.out.println("Creando objeto tipo Estudiante Distancia...");
+                entrada.nextLine(); // Limpiar el buffer del Scanner
+
+                System.out.println("Ingrese nombres:");
+                String nombres = entrada.nextLine();
+                System.out.println("Ingrese apellidos:");
+                String apellidos = entrada.nextLine();
+                System.out.println("Ingrese identificación:");
+                String identificacion = entrada.nextLine();
+                System.out.println("Ingrese edad:");
+                int edad = entrada.nextInt();
+                System.out.println("Ingrese número de asignaturas:");
+                int asignaturas = entrada.nextInt();
+                System.out.println("Ingrese costo asignatura:");
+                double costoAsignatura = entrada.nextDouble();
+
+                EstudianteDistancia estudianteDistancia = new EstudianteDistancia();
+                estudianteDistancia.establecerNombresEstudiante(nombres);
+                estudianteDistancia.establecerApellidoEstudiante(apellidos);
+                estudianteDistancia.establecerEdadEstudiante(edad);
+                estudianteDistancia.establecerIdentificacionEstudiante(identificacion);
+                estudianteDistancia.establecerNumeroAsginaturas(asignaturas);
+                estudianteDistancia.establecerCostoAsignatura(costoAsignatura);
+                estudianteDistancia.calcularMatriculaDistancia();
+
+                System.out.println(estudianteDistancia);
+                break;
+
+            case 2:
+                System.out.println("Creando objeto tipo Estudiante Presencial...");
+                entrada.nextLine(); // Limpiar el buffer del Scanner
+
+                System.out.println("Ingrese nombres:");
+                nombres = entrada.nextLine();
+                System.out.println("Ingrese apellidos:");
+                apellidos = entrada.nextLine();
+                System.out.println("Ingrese identificación:");
+                identificacion = entrada.nextLine();
+                System.out.println("Ingrese edad:");
+                edad = entrada.nextInt();
+                System.out.println("Ingrese número de creditos:");
+                asignaturas = entrada.nextInt();
+                System.out.println("Ingrese costo asignatura:");
+                double costoCredito = entrada.nextDouble();
+
+                EstudiantePresencial estudiantePresencial = new EstudiantePresencial();
+                estudiantePresencial.establecerNombresEstudiante(nombres);
+                estudiantePresencial.establecerApellidoEstudiante(apellidos);
+                estudiantePresencial.establecerEdadEstudiante(edad);
+                estudiantePresencial.establecerIdentificacionEstudiante(identificacion);
+                estudiantePresencial.establecerNumeroCreditos(asignaturas);
+                estudiantePresencial.establecerCostoCredito(costoCredito);
+                estudiantePresencial.calcularMatriculaPresencial();
+
+                System.out.println(estudiantePresencial);
+                break;
+
+            default:
+                System.out.println("Opción no válida");
+        }
     }
 }
